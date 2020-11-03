ad81dbf470e0a5d8b333e6e2b66389feb2a185af
Suleman Butta <sulemanbutta@gmail.com>
2020-11-03 13:42:00 -0600

    added basic navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import LandingPage from './components/LandingPage/LandingPage'
+import Navbar from './components/Navbar/Navbar.js'
 function App() {
   return (
-    <LandingPage/>
+    //<LandingPage/>
+    //<Navbar/>
+    newFunction()
   );
+
+   function newFunction() {
+       return <Navbar />;
+    }
 }
 
 export default App;
--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,1 +1,26 @@
 import React from 'react';
+import {
+    Navbar,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    NavbarText
+  } from 'reactstrap';
+  const example = (props) => {
+//function Navbar(){
+    return(
+    <div>
+        <Navbar color="light" light expand="md">
+            <NavbarBrand href="/">ExploreCities</NavbarBrand>
+            <Nav className="mr-auto" navbar>
+                <NavItem>
+                    <NavLink href="https://github.com/Codubee/ExploreCitiesTTH4-6">GitHub</NavLink>
+                </NavItem>
+            </Nav>
+        <NavbarText>Codubee</NavbarText>
+        </Navbar>
+    </div>
+    )
+}
+export default example;
