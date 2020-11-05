954c19217668023cf41b5ce92e0ebbdaa534580f
Gustavo <gustavooviedo1421@gmail.com>
2020-11-05 12:12:05 -0600

    First push test

--- frontend/src/components/LandingPage/Features/Features.js
+++ frontend/src/components/LandingPage/Features/Features.js
@@ -1,1 +1,13 @@
 import React from 'react'
+
+function Features()
+{
+    return(
+    <div>
+        <h1>Features</h1>
+        <h2>Displays cities located near given zipcode within given mile radius</h2>
+        <h2></h2>
+    </div>
+    );
+}
+export default Features;
