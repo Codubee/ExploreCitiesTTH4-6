d38314965b201d6e9381f4d35bf5547baf192fcc
Gustavo <gustavooviedo1421@gmail.com>
2020-11-06 13:05:24 -0600

    Cleaned up features component. Ready for review.

--- frontend/src/components/LandingPage/Features/Features.js
+++ frontend/src/components/LandingPage/Features/Features.js
@@ -1,14 +1,19 @@
 import React from 'react'
 
+//Displays the features of website to user
 function Features()
 {
     return(
     <div>
         <h1>Features included in our website:</h1>
-            <h2>Displays cities located near given zipcode within given mile radius</h2>
-            <h2>User can select any city listed and explore it</h2>
-                <h3>Common tourist spots and restaurants will be displayed</h3>
-            <h2>Additional information such as flight and Uber costs included</h2>
+            <h3>-Displays cities located near given zipcode and mile radius</h3>
+            <h3>-User can virtually explore any city listed</h3>
+                <h5>*Nearby tourist attractions</h5>
+                <h5>*Restaurants (as well as their ratings!)</h5>
+            <h3>-Additional information provided:</h3>
+                <h5>*Flight costs</h5>
+                <h5>*Uber costs</h5>
+                <h6>*(In case you are interested in visiting!)</h6>
     </div>
     );
 }
