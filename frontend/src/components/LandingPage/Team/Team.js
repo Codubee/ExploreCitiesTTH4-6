8c3dfc0891450194b0ab40de54599ef61c97ad7a
Michael George <michael.george20152@gmail.com>
2020-11-10 15:43:44 -0600

    Renamed TeamCard files to team, changed width of componet using CSS, and edited descriptions

--- frontend/src/components/LandingPage/Team/Team.js
+++ frontend/src/components/LandingPage/Team/Team.js
@@ -1,21 +1,21 @@
 import React from 'react';
+import './Team.css'
 import {
-    Card, CardHeader, CardTitle, CardText, CardDeck, CardBody, CardColumns
+    Card, CardHeader, CardText, CardDeck, CardBody, CardColumns
 } from 'reactstrap';
 
 //Displays the team's information using cards. The cards are contained in a Card deck and arranged by card columns.
-// Cards use info background and border color. Cards have header, title, and text components with decreasing size for each.
+// Cards use info background and border color. Cards have header and text components. Other styling handled by imported CSS file.
 const TeamCardDeck = (props) => {
     return (
-        <CardDeck>
+        <CardDeck class = "Deck">
 
             <CardColumns>
 
                 <Card body inverse color  = "info"> {/* Miguel's card*/}
                     <CardHeader tag = "h2">Team Lead</CardHeader> {/*Position*/}
                     <CardBody>
-                        <CardTitle tag = "h3"> Miguel Zavala</CardTitle> {/*Name*/}
-                        <CardText tag = "h5"> Miguel Zavala is the founder of Codubee and the team's leader and mentor</CardText> {/*Description*/}
+                        <CardText tag = "h5"> Miguel Zavala is the founder of Codubee. Miguel also acts as the team's leader and mentor.</CardText> {/*Description*/}
                     </CardBody>
                 </Card>
 
@@ -23,40 +23,35 @@ const TeamCardDeck = (props) => {
                 <Card body inverse color  = "info"> {/* Aarushi's card*/}
                     <CardHeader tag = "h2">Developer</CardHeader>
                     <CardBody>
-                        <CardTitle tag = "h3">Aarushi Pandey</CardTitle>
-                        <CardText tag = "h5">Aaurushi Pandey is a student at The University of Texas at Dallas and a developer on the team</CardText>
+                        <CardText tag = "h5">Aaurushi Pandey is a Sophomore at The University of Texas at Dallas majoring in Computer Science.</CardText>
                     </CardBody>
                 </Card>
 
                 <Card body inverse color  = "info"> {/* Gustavo's card*/}
                     <CardHeader tag = "h2">Developer</CardHeader>
                     <CardBody>
-                        <CardTitle tag = "h3">Gustavo Oviedo</CardTitle>
-                        <CardText tag = "h5">Gustavo Oviedo is a student at The University of Texas at Dallas and a developer on the team</CardText>
+                        <CardText tag = "h5">Gustavo Oviedo is a Junior at The University of Texas at Dallas majoring in Computer Science.</CardText>
                     </CardBody>
                 </Card>
 
                 <Card body inverse color  = "info"> {/* Jerry's card*/}
                     <CardHeader tag = "h2">Developer</CardHeader>
                     <CardBody>
-                        <CardTitle tag = "h3">Jerry Teng</CardTitle>
-                        <CardText tag = "h5">Jerry Teng is a student at The University of Texas at Dallas and a developer on the team</CardText>
+                        <CardText tag = "h5">Jerry Teng is a Sophomore at The University of Texas at Dallas majoring in Computer Science.</CardText>
                     </CardBody>
                 </Card>
 
                 <Card body inverse color  = "info"> {/* Michael's card*/}
                     <CardHeader tag = "h2">Developer</CardHeader>
                     <CardBody>
-                        <CardTitle tag = "h3">Michael George</CardTitle>
-                        <CardText tag = "h5">Michael George is a student at The University of Texas at Dallas and a developer on the team</CardText>
+                        <CardText tag = "h5">Michael George is a Sophomore at The University of Texas at Dallas majoring in Computer Science.</CardText>
                     </CardBody>
                 </Card>
 
-                <Card body inverse color  = "info"> {/* Sulemanb's card*/}
+                <Card body inverse color  = "info"> {/* Suleman's card*/}
                     <CardHeader tag = "h2">Developer</CardHeader>
                     <CardBody>
-                        <CardTitle tag = "h3">Sulemanb Butta</CardTitle>
-                        <CardText tag = "h5">Sulemanb Butta is a student at The University of Texas at Dallas and a developer on the team</CardText>
+                        <CardText tag = "h5">Suleman Butta is a Junior at The University of Texas at Dallas majoring in Computer Science.</CardText>
                     </CardBody>
                 </Card>
 
