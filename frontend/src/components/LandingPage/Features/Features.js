d92c15894ee55a213ca121d989f54b701ef0dfb8
Gustavo <gustavooviedo1421@gmail.com>
2020-11-12 12:25:58 -0600

    Styling added

--- frontend/src/components/LandingPage/Features/Features.js
+++ frontend/src/components/LandingPage/Features/Features.js
@@ -1,20 +1,19 @@
 import React from 'react'
 import './Features.css'
 
-//Displays the features of website to user
 function Features()
 {
     return(
-    <div class = "container">
+    <div class = "container timesnewroman bgcolor spacing">
         <h1 className = "header">Features included in our website</h1>
-            <h3 className = "tab">-Displays cities located near given zipcode and mile radius</h3>
-            <h3 className = "tab">-User can virtually explore any city listed</h3>
-                <h5 className = "doubletab">*Nearby tourist attractions</h5>
-                <h5 className = "doubletab">*Restaurants (as well as their ratings!)</h5>
-            <h3 className = "tab">-Additional information provided:</h3>
-                <h5 className = "doubletab">*Flight costs</h5>
-                <h5 className = "doubletab">*Uber costs</h5>
-                    <h6 className = "doubletab">*(In case you are interested in visiting!)</h6>
+            <h4 className = "tab">- Displays cities located near given zipcode and mile radius</h4>
+            <h4 className = "tab">- User can virtually explore any city listed</h4>
+                <p className = "doubletab">* Nearby tourist attractions</p>
+                <p className = "doubletab">* Restaurants (as well as their ratings!)</p>
+            <h4 className = "tab">- Additional information provided:</h4>
+                <p className = "doubletab">* Flight costs</p>
+                <p className = "doubletab">* Uber costs</p>
+                    <p className = "doubletab">(In case you are interested in visiting!)</p>
     </div>
     );
 }
