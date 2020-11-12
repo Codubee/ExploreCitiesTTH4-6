835b994cc35c91129f2c89445ff6f13765c07007
Gustavo <gustavooviedo1421@gmail.com>
2020-11-12 11:44:34 -0600

    Centered text

--- frontend/src/components/LandingPage/Features/Features.js
+++ frontend/src/components/LandingPage/Features/Features.js
@@ -1,19 +1,20 @@
 import React from 'react'
+import './Features.css'
 
 //Displays the features of website to user
 function Features()
 {
     return(
-    <div>
-        <h1>Features included in our website:</h1>
-            <h3>-Displays cities located near given zipcode and mile radius</h3>
-            <h3>-User can virtually explore any city listed</h3>
-                <h5>*Nearby tourist attractions</h5>
-                <h5>*Restaurants (as well as their ratings!)</h5>
-            <h3>-Additional information provided:</h3>
-                <h5>*Flight costs</h5>
-                <h5>*Uber costs</h5>
-                    <h6>*(In case you are interested in visiting!)</h6>
+    <div class = "container">
+        <h1 className = "header">Features included in our website</h1>
+            <h3 className = "tab">-Displays cities located near given zipcode and mile radius</h3>
+            <h3 className = "tab">-User can virtually explore any city listed</h3>
+                <h5 className = "doubletab">*Nearby tourist attractions</h5>
+                <h5 className = "doubletab">*Restaurants (as well as their ratings!)</h5>
+            <h3 className = "tab">-Additional information provided:</h3>
+                <h5 className = "doubletab">*Flight costs</h5>
+                <h5 className = "doubletab">*Uber costs</h5>
+                    <h6 className = "doubletab">*(In case you are interested in visiting!)</h6>
     </div>
     );
 }
