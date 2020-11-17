e563f0c897473f3ba87ba349b4e27768a4a9cd89
Michael George <michael.george20152@gmail.com>
2020-11-17 15:06:18 -0600

    updated landingpage to resolve merge conflicts

--- frontend/src/components/LandingPage/LandingPage.js
+++ frontend/src/components/LandingPage/LandingPage.js
@@ -1,13 +1,15 @@
 import React from 'react'
+import Explanation from './Explanation/Explanation.js'
 import Tools from './Tools/Tools'
 import Team from './Team/Team'
 
 function LandingPage(){
     return(
-            <div>
+            <>
+            <Explanation/>
             <Tools/>
             <Team/>
-            </div>
+            </>
     )
 }
 
