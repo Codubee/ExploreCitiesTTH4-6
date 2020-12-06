f1126e2bc89d977a8420410594c04ae9d8db4a14
Aarushi Pandey <pandey.aarushi2010@gmail.com>
2020-12-06 00:32:55 -0600

    added input for distance user wants to travel

--- frontend/src/components/LandingPage/Zipcode/Zipcode.js
+++ frontend/src/components/LandingPage/Zipcode/Zipcode.js
@@ -55,15 +55,21 @@ class Zipcode extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            zipcode: null
+            zipcode: null,
+            distance: null
         };
 
-        this.handleChange = this.handleChange.bind(this);
+        this.handleZipcodeChange = this.handleZipcodeChange.bind(this);
+        this.handleDistanceChange = this.handleDistanceChange.bind(this);
         //this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleChange(event) {
+    handleZipcodeChange(event) {
         this.setState({zipcode: event.target.value});
+    }
+
+    handleDistanceChange(event) {
+        this.setState({distance: event.target.value});
     }
 
    /* handleSubmit(event) {
@@ -75,19 +81,28 @@ class Zipcode extends React.Component {
         };
     }*/
 
+    /*handleSubmit(event) {
+        this.handleDistanceChange(event);
+        this.handleZipcodeChange(event);
+    }*/
+
     render() {
         return(
             <Jumbotron className="container">
-                <h2>Enter your zip code:&nbsp;</h2>
                 <form className="form" noValidate autoComplete="off">
-                    <TextField id="outlined-basic" className={useStyles.margin} label="Zip code" variant="outlined" size="small" onChange={this.handleChange} value={this.state.zipcode} />
+                    <label><h2>Enter your zip code:&nbsp;</h2></label>
+                    <TextField id="outlined-basic" className={useStyles.margin} label="Zip code" variant="outlined" size="small" onChange={this.handleZipcodeChange} value={this.state.zipcode} />
+                    &nbsp;&nbsp; <br />
+                    <label><h2>Enter how far you want to travel:&nbsp;</h2></label>
+                    <TextField id="outlined-basic" className={useStyles.margin} label="Distance (in km)" variant="outlined" size="small" onChange={this.handleDistanceChange} value={this.state.distance} />
                     &nbsp;&nbsp;
+                    <br />
                     <Button variant="contained" className={useStyles.margin} color="primary" size="medium" /*onClick={this.handleSubmit}*/ >
                         Submit
                     </Button> 
                 </form>
                 <div className="break">
-                    <h2>Zipcode entered is: {this.state.zipcode} </h2>
+                    <h2>Zipcode entered is: {this.state.zipcode}, Distance entered is: {this.state.distance} </h2>
                 </div>
             </Jumbotron>
         );
