11cf0f74e0d7b68ff1068062bb6278edfbf09ce1
Aarushi Pandey <pandey.aarushi2010@gmail.com>
2020-12-14 15:34:09 -0800

    called zipcode api in localhost 8080, unable to pass user zipcode and distance from the user input component

--- backend/index.js
+++ backend/index.js
@@ -6,12 +6,33 @@ app.use(express.json())
 
 const port = 8080;
 
-app.get('/',(req,res)=>{
-    res.send('Hello world');
+/*var zipcode = sessionStorage.getItem("zipcode");
+console.log(zipcode);
+var zipcode = sessionStorage.getItem("distance");
+console.log(distance);*/
+
+const zipcode = 75080;
+const distance = 50;
+
+app.get('/getZipcodes',(req,res)=>{
+    //res.send('Hello world');
+
+    const headers = {headers: {authorization: process.env.API_KEY}}
+
+    axios.get('https://www.zipcodeapi.com/rest/radius.json/'+zipcode+'/'+distance+'/km')
+    .then((response) => {
+        console.log(response)
+        res.status(200).json(response.data)
+    })
+    .catch((error) => {
+        console.log(error);
+        res.status(400).json({error: "An error occured"})
+    })
 })
 
 app.listen(port,()=>{
     console.log('API is up and running')
+
 })
 
 module.exports = app;
