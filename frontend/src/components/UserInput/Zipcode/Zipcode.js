be84e271d6e56b198645269fab37e23019aab99f
Aarushi Pandey <pandey.aarushi2010@gmail.com>
2020-12-17 14:55:55 -0800

    Fixed css naming issue, uncommented handleSubmit function and deleted unused code

--- frontend/src/components/UserInput/Zipcode/Zipcode.js
+++ frontend/src/components/UserInput/Zipcode/Zipcode.js
@@ -1,19 +1,8 @@
 import React from 'react'
 import "./Zipcode.css"
-//import { Form, Input, Button } from 'semantic-ui-react'
-import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import { Jumbotron } from 'reactstrap'
 import Button from '@material-ui/core/Button';
-
-const useStyles = makeStyles((theme) => ({
-    margin: {
-      margin: theme.spacing(1),
-    },
-    extendedIcon: {
-      marginRight: theme.spacing(1),
-    },
-  }));
 
 class Zipcode extends React.Component {
     constructor(props) {
@@ -25,7 +14,7 @@ class Zipcode extends React.Component {
 
         this.handleZipcodeChange = this.handleZipcodeChange.bind(this);
         this.handleDistanceChange = this.handleDistanceChange.bind(this);
-        //this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleZipcodeChange(event) {
@@ -36,36 +25,26 @@ class Zipcode extends React.Component {
         this.setState({distance: event.target.value});
     }
 
-   /* handleSubmit(event) {
-        this.setState({zipcode: event.target.value});
-        return () => {
-            <div className="break">
-                    <h2>Zipcode entered is: {this.state.zipcode} </h2>
-                </div>
-        };
-    }*/
-
-    /*handleSubmit(event) {
-        this.handleDistanceChange(event);
-        this.handleZipcodeChange(event);
-    }*/
+    handleSubmit(event) {
+        //will make zipcode api call here and then route to another component
+    }
 
     render() {
         return(
-            <Jumbotron className="container">
-                <form className="form" noValidate autoComplete="off">
+            <Jumbotron className="zipcode-container">
+                <form className="zipcode-form" noValidate autoComplete="off">
                     <label><h2>Enter your zip code:&nbsp;</h2></label>
-                    <TextField id="outlined-basic" className={useStyles.margin} label="Zip code" variant="outlined" size="small" onChange={this.handleZipcodeChange} value={this.state.zipcode} />
+                    <TextField id="outlined-basic" label="Zip code" variant="outlined" size="small" onChange={this.handleZipcodeChange} value={this.state.zipcode} />
                     &nbsp;&nbsp; <br />
                     <label><h2>Enter how far you want to travel:&nbsp;</h2></label>
-                    <TextField id="outlined-basic" className={useStyles.margin} label="Distance (in km)" variant="outlined" size="small" onChange={this.handleDistanceChange} value={this.state.distance} />
+                    <TextField id="outlined-basic" label="Distance (in km)" variant="outlined" size="small" onChange={this.handleDistanceChange} value={this.state.distance} />
                     &nbsp;&nbsp;
                     <br />
-                    <Button variant="contained" className={useStyles.margin} color="primary" size="medium" /*onClick={this.handleSubmit}*/ >
+                    <Button variant="contained" color="primary" size="medium" onClick={this.handleSubmit} >
                         Submit
                     </Button> 
                 </form>
-                <div className="break">
+                <div className="zipcode-break">
                     <h2>Zipcode entered is: {this.state.zipcode}, Distance entered is: {this.state.distance} </h2>
                 </div>
             </Jumbotron>
