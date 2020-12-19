bd81f9cc4a234992afb971dfafd8d0f7e3b58b67
sulemanbutta <62159370+sulemanbutta@users.noreply.github.com>
2020-12-19 16:02:56 -0600

    created get function to call yelp resturant api (#12)
    Co-authored-by: Suleman Butta <sulemanbutta@gmail.com>

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,23 @@ app.get('/',(req,res)=>{
     res.send('Hello world');
 })
 
+app.get('/restaurants/:zip', (req, res) => {
+
+    console.log(req.params.name)
+    let zip = req.params.zip;
+    const headers = { headers: { 'Authorization': 'Bearer '+process.env.YELP_KEY}}
+
+    axios.get('https://api.yelp.com/v3/businesses/search?term=restaurants&location='+zip, headers)
+    .then(function (response) {
+        console.log(response);
+        res.status(200).json(response.data)
+    })
+    .catch(function (error) {
+        console.log(error);
+        res.status(400).json({error:"An error occurred"});
+    })
+})
+
 app.listen(port,()=>{
     console.log('API is up and running')
 })
