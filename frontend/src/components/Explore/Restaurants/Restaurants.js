faefff7d393ee2f88014d3c9cf661be590ae8c1e
Jerry Teng <zuyuangteng@gmail.com>
2021-01-02 00:29:33 -0600

    restaurant added component

--- frontend/src/components/Explore/Restaurants/Restaurants.js
+++ frontend/src/components/Explore/Restaurants/Restaurants.js
@@ -1,40 +1,94 @@
 import React from "react";
 import "./Restaurants.css";
 import axios from "axios";
+import { withRouter } from "react-router";
+import {
+	Card,
+	CardImg,
+	CardBody,
+	CardTitle,
+	CardText,
+	CardSubtitle,
+	Button,
+} from "reactstrap";
 
 class Restaurants extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			zip: props.match.params.zip,
+			zip: this.props.match.params.zip,
 			restaurants: [],
 		};
 	}
 
 	componentDidMount() {
-		console.log(this.state.zip);
 		axios
 			.get("/restaurants/" + this.state.zip)
 			.then((response) => {
-				console.log(response);
 				this.setState({
-					restaurants: response.data.restaurants,
+					restaurants: response.data.businesses,
 				});
 			})
-			.catch((error) => {
-				console.log(error);
+			.catch((err) => {
+				console.error(err);
 			});
 	}
-
 	render() {
 		return (
-			<div>
+			// 	<div>
+			// 		{this.state.restaurants.map((restaurant) => (
+			// 			<div key={restaurant.id}>
+			// 				<img
+			// 					src={restaurant.image_url}
+			// 					alt=""
+			// 					width="30%"
+			// 					height="auto"
+			// 				/>
+			// 				{/* <div>{restaurant.name}</div> */}
+			// 			</div>
+			// 		))}
+			// 		{/* {this.state.restaurants.map((restaurant) => (
+			// 			<div key={restaurant.id}>
+			// 				<img src={restaurant.image_url} alt="" />
+			// 			</div>;
+			// ))} */}
+			// 	</div>
+
+			<div className="card-container">
 				{this.state.restaurants.map((restaurant) => (
-					<div>{restaurant.name}</div>
+					<div key={restaurant.id}>
+						<Card className="restaurant">
+							<CardImg
+								src=""
+								alt="restaurant image"
+								width="auto"
+								height="auto"
+							/>
+							<CardBody>
+								<CardTitle tag="h5">
+									{restaurant.name}
+								</CardTitle>
+								<CardSubtitle
+									tag="h6"
+									className="mb-2 text-muted"
+								>
+									{restaurant.location.display_address}
+								</CardSubtitle>
+								<CardText>{restaurant.display_phone}</CardText>
+								<Button
+									color="danger"
+									href={restaurant.url}
+									target="_blank"
+								>
+									Yelp
+								</Button>
+							</CardBody>
+						</Card>
+					</div>
 				))}
 			</div>
 		);
 	}
 }
 
-export default Restaurants;
+export default withRouter(Restaurants);
