4c618bc8fc340f14f5801d3d1c60141dc80805c6
Jerry Teng <zuyuangteng@gmail.com>
2021-01-08 17:43:10 -0600

    removed cities from userinput page

--- frontend/src/components/UserInput/UserInput.js
+++ frontend/src/components/UserInput/UserInput.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import Zipcode from './Zipcode/Zipcode'
-import Cities from './Cities/Cities'
 
 function UserInput(){
     return(
       <>
         <Zipcode/>
-        <Cities />
       </>
     )
 }
